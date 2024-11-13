a4d357fdb3d16ae306bb2879efa0b1f080ac5079
FoKey <ndk52512@gmail.com>
2024-11-13 21:23:09 +0300

    8

--- src/router.rs
+++ src/router.rs
@@ -9,27 +9,30 @@ use crate::controllers::update_app;
 #[derive(Deserialize)]
 pub struct Info {
   id: i32,
-  function: i32,
-}
-
-pub async fn distribution(info: &Info) {
-  match info.function {
-    0 => { save_wallpaper::save().await.expect("Error save_wallpaper in router"); },
-    1 => { change_wallpaper::change().await.expect("Error change_wallpaper in router"); },
-    2 => { reset_wallpaper::reset().await.expect("Error reset_wallpaper in router"); },
-    3 => { update_app::update().await.expect("Error update_app in router"); },
-    _ => todo!(),  
-  }  
+  function: i32
 }
 
 pub async fn api_post(info: web::Json<Info>) -> Result<String, Error> {
-  distribution(&info).await;
-  Ok("Request post ok".to_string())
+  match info.function {
+    1 => { save_wallpaper::save().await.expect("Error save_wallpaper in router"); Ok("1".to_string())},
+    2 => { change_wallpaper::change().await.expect("Error change_wallpaper in router"); Ok("2".to_string())},
+    3 => { reset_wallpaper::reset().await.expect("Error reset_wallpaper in router");Ok("3".to_string()) },
+    4 => { update_app::update().await.expect("Error update_app in router"); Ok("4".to_string())},
+    _ => todo!(),  
+  }
 }
 
 pub async fn api_get(info: web::Json<Info>) -> Result<String, Error> {
-  distribution(&info).await;
-  Ok("request get ok".to_string())
+  let id = info.id as usize;
+  let function = info.function as usize;
+  let mut clients = [[false; 4]; 4];
+  
+  if id < clients.len() && function < clients[id].len() {
+    clients[id][function] = true; 
+    println!("True {} {}", id, function);
+  }
+
+  Ok("Ok".to_string())
 }
 
 pub fn api_scope() -> Scope {
